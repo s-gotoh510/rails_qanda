f883822fba5ab207db205fe19d4c2ee017622bdf
s-gotoh <shinya510goto@gmail.com>
2021-04-13 19:44:26 +0900

    create and edit new.html.erb

--- Gemfile
+++ Gemfile
@@ -62,3 +62,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# bootstrap
+gem 'bootstrap', '~> 4.1.1'
+#jquery
+gem 'jquery-rails', '~> 4.3.1'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(id: 1, name: '花崎かれん', title: '日程調整について', content: '今月いっぱいだった課題提出期限の延長をします。')
+Question.create(id: 2, name: '太田憲治', title: '通路拡張工事のお知らせ', content: '4階以上にお住まいの皆様に影響がでます。')
+Question.create(id: 3, name: '野口一郎', title: '卒業コンペの会場変更のお知らせ', content: '新型コロナウィルスの影響で、会場が変更になりました。')
